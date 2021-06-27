7d1a48f7f481d33e9eaf2397a9e740ac587b2381
Henrique Augusto <73003917+0Augusto@users.noreply.github.com>
2021-06-27 14:39:50 -0300

    Add files via upload

---
+++ python/inicio/tipo.py
@@ -0,0 +1,20 @@
+#Nao precisa indicar o tipo da variavel em Python, a propria linguagem constroi a mais adequada
+
+a = 2
+print(type(a))
+#Output: <class 'int'>
+b = 987746463737885857637
+print(type(b))
+#Output: <class 'int'>
+pi = 3.14
+print(type(pi))
+#Output: <class 'float'>
+c = 'A'
+print(type(c))
+#Output: <class 'str'> str = string
+q = True
+print(type(q))
+#Output: <class 'bool'>
+x = None
+print(type(x))
+#Output: <class 'NoneType'>
---
+++ python/inicio/tupla.py
@@ -0,0 +1,7 @@
+a = [1, 2, 3]
+
+b = ['a', 1, 'python', [1, 2]] #permitido
+#substitui o que que esta na posicao [2] (python) por (something else)
+b[2] = 'something else'
+
+print(b)
